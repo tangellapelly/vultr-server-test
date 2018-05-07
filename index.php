4f95312ce73a4775f132735ca070fb9b062d75e5
Sudheer Tangellapelly <tangellapelly@gmail.com>
2018-05-07 23:29:24 +0800

    added the error_reporting to view the errors on the browser

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-
+error_reporting(1);
  require 'vendor/autoload.php';
  $collection = (new MongoDB\Client("mongodb://127.0.0.1:27017"))->test->movie;
 
@@ -15,10 +15,10 @@ $collection->insert($document);
 // find everything in the collection
 $cursor = $collection->find();
 
-// iterate through the results
+/* iterate through the results
 foreach ($cursor as $document) {
     echo $document["title"] . "\n";
-}
+}*/
 
 ?>
 

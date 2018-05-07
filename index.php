0b1157b509d0b5aa79a3b9fe47f18f7c4e6d29bf
Sudheer Tangellapelly <tangellapelly@gmail.com>
2018-05-07 23:55:31 +0800

    added bootstrap template to render the table

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 <?php
 
 require_once('dbConfig.php');
@@ -25,7 +32,24 @@ $cursor = $manager->executeQuery('onlinestore.products', $query);
 
 
 ?>
-<table class='table table-bordered'>
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+    <title>Produts table</title>
+  </head>
+  <body>
+  <table class='table table-bordered'>
    <thead>
 
       <tr>
@@ -74,3 +98,11 @@ $cursor = $manager->executeQuery('onlinestore.products', $query);
   ?>
 
 </table>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>

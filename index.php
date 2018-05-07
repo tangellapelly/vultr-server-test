ea03411b8d5592cda66b7ef313c862f7a5a378fd
Sudheer Tangellapelly <tangellapelly@gmail.com>
2018-05-07 23:50:49 +0800

    added the table to display the products fetched from db

--- index.php
+++ index.php
@@ -22,4 +22,54 @@ $query = new MongoDB\Driver\Query($filter, $options);
 
 $cursor = $manager->executeQuery('onlinestore.products', $query);
 
+
+<table class='table table-bordered'>
+   <thead>
+
+      <tr>
+            <th>#</th>
+
+            <th>Prodcut</th>
+
+            <th>Price</th>
+
+             <th>Category</th>
+    
+            <th>Action</th>
+
+      </tr>
+  
+   </thead>
+
+    <?php 
+
+    $i =1; 
+
+    foreach ($cursor as $document) {   ?>
+
+      <tr>
+
+      <td><?php echo $i; ?></td>
+
+      <td><?php echo $document->product_name;  ?></td>
+
+      <td><?php echo $document->price;  ?></td>        
+    
+     <td><?php echo $document->category;  ?></td>
+      
+     <td><a class='editlink' data-id=<?php echo $document->_id; ?> 
+             href='javascript:void(0)'>Edit</a> |
+        <a onClick ='return confirm("Do you want to remove this
+                     record?");' 
+        href='record_delete.php?id=<?php echo $document->_id;  ?>'>Delete</td>
+
+      </tr>
+
+     <?php $i++;  
+
+  } 
+
+  ?>
+
+</table>
 ?>
